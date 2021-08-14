01cd532a8e9e683e028d9af6335e09ac11f3229f
Mark Flamer <mflamer@users.noreply.github.com>
2021-08-14 14:12:07 -0700

    Revert "Add basic mesh loader functions"
    This reverts commit 7433b9c54a5cfe17a7cd7d25c7702aee2f83cf90.

--- plib.h
+++ plib.h
@@ -9,9 +9,9 @@ struct V2{
 			V2(){;}
 			V2(float _x, float _y){x = _x; y = _y;}
 	float 	Distance(const V2& v);
-	float 	Mag() const;
+	float 	Mag();
 	float 	Dot(const V2& v);
-	V2	  	Unit() const ;
+	V2	  	Unit();
 	V2		operator-();
 
 	float x;
@@ -88,8 +88,6 @@ public:
 	void	Step(float t);
 	void	Collisions();
 
-	int		SizeNodes(){return nodes.size();}
-	Node*	GetNodeIdx(int i){return nodes.at(i);}
 
 	void 	MapNodes(NodeFunct* f);
 	void 	MapBars(BarFunct* f);
